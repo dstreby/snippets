6d7153fb2b01f05a12b7d4b8cf855c7835bce17b
Daniel Streby <dstreby@2u.com>
2016-05-13 16:45:11 -0400

    Updating bash/sanity.sh file iteration snippet to use alternate file descriptor so looped commands (e.g. ssh) don't eat stdin

--- bash/sanity.sh
+++ bash/sanity.sh
@@ -10,9 +10,13 @@ readonly SCRIPT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 readonly ARGS="$@"
 
 # Iterate through a file (line-by-line):
-while IFS='' read -r line || [[ -n "$line" ]]; do
+# N.B.: bash 4.1+ required for automatic file descriptor allocation
+# replace $READ_FD / {READ_FD} with arbitrary file descriptor for older / BSD
+# bash.
+readonly IN_FILE=${1:?}
+while IFS='' read -u $READ_FD -r line || [[ -n "$line" ]]; do
   echo "Text read from file: $line"
-done < "$filename"
+done {READ_FD}< "$IN_FILE"
 
 ################################################################################
 # Helper functions - useful in many scripts                                    #
